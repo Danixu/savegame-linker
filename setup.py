da44c72f3679c779024ebce15e5519ba30e58dcf
Daniel <danielmadrid19@gmail.com>
2019-02-24 18:20:19 +0100

    Changes to make the builded version smaller
    Added some modules to zip packages changes the size of pachage from 95Mb to 37Mb.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ __version__ = "0.5.0"
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 include_files = ['images', 'audio']
-excludes = ["OpenGL", "email", "distutils", "html", "pydoc_data", "unittest", "http", "xml"]
-packages = ["widgets", "windows", "ctypes"]
+excludes = ["OpenGL", "email", "distutils", "html", "pydoc_data", "unittest", "http", "xml", "pkg_resources"]
+packages = ["widgets", "windows", "ctypes", "json", "logging", "PIL", "pygame", "sqlite3", "urllib", "wx", "importlib", "encodings", "collections"]
 
 setup (
   name = "appname",
